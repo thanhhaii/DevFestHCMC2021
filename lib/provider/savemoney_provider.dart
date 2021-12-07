f399b208a4b1c3957921bee4dadc373f83e975de
thanhaii <hailam@infevo.net>
2021-12-08 02:02:14 +0700

    develop sign in, sign out, create action

--- lib/provider/savemoney_provider.dart
+++ lib/provider/savemoney_provider.dart
@@ -1,4 +1,4 @@
-
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,40 +6,64 @@ import 'package:final_project_devfest/model/moneysave.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
-class SaveMoneyProvider with ChangeNotifier{
+class SaveMoneyProvider with ChangeNotifier {
   List<MoneySave> lsMoneySave = [];
-  Future<void> getUserDataById() async{
-    dynamic currentUser = FirebaseAuth.instance.currentUser;
-    QuerySnapshot snapshot = await FirebaseFirestore.instance.doc("a9TIlX1NeBKPnyrfDeNO").collection("MoneySave").get();
-    for (var element in snapshot.docs) {
 
-      }
-    }
-  Future<void> createUserData(MoneySave moneySave) async{
+  Future<void> getUserDataById() async {
     dynamic currentUser = FirebaseAuth.instance.currentUser;
-    dynamic document = await FirebaseFirestore.instance.doc(currentUser.uid).collection("MoneySave").doc();
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .doc("a9TIlX1NeBKPnyrfDeNO")
+        .collection("MoneySave")
+        .get();
+    for (var element in snapshot.docs) {}
+  }
+
+  Future<bool> createUserData(MoneySave moneySave) async {
+    var result = false;
     try {
-      await document.set(moneySave).whenComplete(()=>print("Create Complete"));
+      dynamic currentUser = FirebaseAuth.instance.currentUser;
+      dynamic document = FirebaseFirestore.instance
+          .collection("MoneySave")
+          .doc(currentUser.uid);
+      Map<String, dynamic> map = {};
+      map["title"] = moneySave.title;
+      map["money"] = moneySave.money;
+      map["isPay"] = moneySave.isPay;
+      map["date"] = moneySave.date;
+      map["userUID"] = moneySave.userUID;
+      await document.set(map).whenComplete(() => {result = true});
+    } catch (e) {
+      result = false;
+    }
+    return result;
+  }
+
+  Future<void> updateUserData(MoneySave moneySave, String docId) async {
+    dynamic currentUser = FirebaseAuth.instance.currentUser;
+    // dynamic document = await FirebaseFirestore.instance
+    //     .doc(currentUser.uid)
+    //     .collection("MoneySave")
+    //     .doc(docId);
+    try {
+      Map<String, dynamic> map = json.decode(moneySave.toString());
+      // await document
+      //     .update(map)
+      //     .whenComplete(() => print("Create Complete"));
     } catch (e) {
       print(e);
     }
   }
-  Future<void> updateUserData(MoneySave moneySave, String docId) async{
-  dynamic currentUser = FirebaseAuth.instance.currentUser;
-  dynamic document = await FirebaseFirestore.instance.doc(currentUser.uid).collection("MoneySave").doc(docId);
-  try {
-    await document.update(moneySave).whenComplete(()=>print("Create Complete"));
-  } catch (e) {
-    print(e);
+
+  static Future<void> deleteUserData(MoneySave moneySave, String docId) async {
+    dynamic currentUser = FirebaseAuth.instance.currentUser;
+    dynamic document = await FirebaseFirestore.instance
+        .doc(currentUser.uid)
+        .collection("MoneySave")
+        .doc(docId);
+    try {
+      await document.delete().whenComplete(() => print("Create Complete"));
+    } catch (e) {
+      print(e);
+    }
   }
-}
-static Future<void> deleteUserData(MoneySave moneySave, String docId) async{
-  dynamic currentUser = FirebaseAuth.instance.currentUser;
-  dynamic document = await FirebaseFirestore.instance.doc(currentUser.uid).collection("MoneySave").doc(docId);
-  try {
-    await document.delete().whenComplete(()=>print("Create Complete"));
-  } catch (e) {
-    print(e);
-  }
-}
 }
--- lib/screen/action/action.dart
+++ lib/screen/action/action.dart
@@ -1,6 +1,13 @@
 import 'package:final_project_devfest/components/appbarcomponent.dart';
+import 'package:final_project_devfest/model/moneysave.dart';
+import 'package:final_project_devfest/provider/savemoney_provider.dart';
+import 'package:final_project_devfest/screen/home/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 
 class ActionPage extends StatefulWidget {
   const ActionPage({Key? key}) : super(key: key);
@@ -11,68 +18,179 @@ class ActionPage extends StatefulWidget {
 
 class _ActionPageState extends State<ActionPage> {
   TextEditingController amountOfMoneyTextField = TextEditingController();
-  String _type = "revenue";
-  TextEditingController descriptionTextField = TextEditingController();
+  String _type = "expense";
+  TextEditingController titleTextField = TextEditingController();
+  DateTime timeFocus = DateTime.now();
+  TextEditingController dateInput = TextEditingController(
+      text: DateFormat("dd-MM-yyyy").format(DateTime.now()));
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _createAction() async {
+    final FormState? _form = _formKey.currentState;
+    if (_form!.validate()) {
+      SaveMoneyProvider moneyProvider = SaveMoneyProvider();
+      dynamic currentUser = FirebaseAuth.instance.currentUser;
+      var moneySave = MoneySave(
+          titleTextField.text.toString(),
+          double.parse(amountOfMoneyTextField.text.toString()),
+          _type == "expense",
+          timeFocus,
+          currentUser.uid);
+      var result = await moneyProvider.createUserData(moneySave);
+      if(result){
+        Navigator.pop(context);
+        Fluttertoast.showToast(
+            msg: "Create action success!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }else {
+        Fluttertoast.showToast(
+            msg: "An error occurred, please try again later!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Amount of money"),
-                  controller: amountOfMoneyTextField,
-                )),
-            Row(
-              children: [
-                Expanded(
-                    child: RadioListTile(
-                        value: "revenue",
-                        title: const Text("Revenue"),
-                        groupValue: _type,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _type = newValue!;
-                          });
-                        })),
-                Expanded(
-                    child: RadioListTile(
-                        value: "expenditure",
-                        title: const Text("Expenditure"),
-                        groupValue: _type,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _type = newValue!;
-                          });
-                        }))
-              ],
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Description",
-              ),
-              controller: descriptionTextField,
-              maxLines: 4,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                child: const Text("Save"),
-                onPressed: () {},
-              ),
-            )
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.title),
+                      hintText: "Title",
+                    ),
+                    validator: (String? title) {
+                      if (title!.isEmpty) {
+                        return "Please fill title!";
+                      }
+                    },
+                    controller: titleTextField,
+                    maxLines: 1,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: TextFormField(
+                        validator: (String? money) {
+                          if (money!.isEmpty) {
+                            return "Please fill amount of money!";
+                          }
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            icon: Icon(Icons.attach_money),
+                            hintText: "Amount of money"),
+                        controller: amountOfMoneyTextField,
+                      )),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Row(
+                        children: [
+                          Radio(
+                              value: "expense",
+                              groupValue: _type,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  _type = newValue!;
+                                });
+                              }),
+                          const Text(
+                            "Expense",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      )),
+                      Expanded(
+                          child: Row(
+                        children: [
+                          Radio(
+                              value: "income",
+                              groupValue: _type,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  _type = newValue!;
+                                });
+                              }),
+                          const Text(
+                            "Income",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ))
+                    ],
+                  ),
+                  TextField(
+                    controller: dateInput,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.calendar_today),
+                        //icon of text field
+                        labelText: "Enter Date" //label text of field
+                        ),
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          //DateTime.now() - not to allow to choose before today.
+                          lastDate: DateTime(2101));
+
+                      if (pickedDate != null) {
+                        timeFocus = pickedDate;
+                        String formattedDate =
+                            DateFormat("dd-MM-yyyy").format(pickedDate);
+                        setState(() {
+                          dateInput.text =
+                              formattedDate; //set output date to TextField value.
+                        });
+                      } else {
+                        print("Date is not selected");
+                      }
+                    },
+                  ),
+                ],
+              )),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: const Text("Save"),
+                  onPressed: () {
+                    _createAction();
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(55),
-        child: AppBarComponent(title: "Action Page"),
+      appBar: AppBar(
+        title: const Text("Action"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
       ),
     );
   }
